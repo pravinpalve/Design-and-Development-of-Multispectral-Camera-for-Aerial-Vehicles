38de9a47bb8c526fb02f398ebf64815bd970ac9f
Gokul Edakke Puram <ge-235725@hs-weingarten.de>
2024-06-18 20:20:06 +0200

    Splitting images into folders camera-wise

--- src/splitimg.py
+++ src/splitimg.py
@@ -3,7 +3,7 @@ import numpy as np
 
 root = '/Users/gokulmnambiar/Desktop/GitHubRepos/Design-and-Development-of-Multispectral-Camera-for-Aerial-Vehicles/CalibImgs'
 
-for i in range(10):
+for i in range(14):
 
     img = cv2.imread(f'{root}/calib{i+1}.jpg')
 
@@ -14,10 +14,10 @@ for i in range(10):
     img2 = img[:,2560:3840,:]
     img3 = img[:,3840:5120,:]
 
-    cv2.imwrite(f'{root}/calib{i+1}/0.jpg', img0)
-    cv2.imwrite(f'{root}/calib{i+1}/1.jpg', img1)
-    cv2.imwrite(f'{root}/calib{i+1}/2.jpg', img2)
-    cv2.imwrite(f'{root}/calib{i+1}/3.jpg', img3)
+    cv2.imwrite(f'{root}/NewCalibImgs/Cam0/{i+1}.jpg', img0)
+    cv2.imwrite(f'{root}/NewCalibImgs/Cam1/{i+1}.jpg', img1)
+    cv2.imwrite(f'{root}/NewCalibImgs/Cam2/{i+1}.jpg', img2)
+    cv2.imwrite(f'{root}/NewCalibImgs/Cam3/{i+1}.jpg', img3)
 #cv2.imshow('img0',img0)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
